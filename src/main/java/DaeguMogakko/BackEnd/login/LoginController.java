db1ac4cb6a56930375a2541669d0227e11565758
sami355-24 <ddung355@gmail.com>
2024-07-27 18:33:17 +0900

    [Feat]: 멤버 로그인 로직 컨트롤러 @RequestBody 어노테이션 추가

--- src/main/java/DaeguMogakko/BackEnd/login/LoginController.java
+++ src/main/java/DaeguMogakko/BackEnd/login/LoginController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(LoginDto loginDto) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDto loginDto) {
         loginService.isCorrectEmail(loginDto.email());
         loginService.isCorrectPassword(loginDto.email(), loginDto.password());
         String accessToken = loginService.issueAccessToken(loginDto.email());
