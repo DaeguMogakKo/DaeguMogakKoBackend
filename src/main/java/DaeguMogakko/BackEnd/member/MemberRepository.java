5473d8f6c1b962ebdea59aa103c2c14c265c4447
sami355-24 <ddung355@gmail.com>
2024-07-27 18:06:58 +0900

    [Feat]: 멤버 레포지토리 메소드 추가

--- src/main/java/DaeguMogakko/BackEnd/member/MemberRepository.java
+++ src/main/java/DaeguMogakko/BackEnd/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package DaeguMogakko.BackEnd.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
