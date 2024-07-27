9944cfe59994533b73235d516b2f63c01109dc5f
sami355-24 <ddung355@gmail.com>
2024-07-27 16:43:10 +0900

    [Feat]: Security내부에서 사용하는 인증 클래스 생성

---
+++ src/main/java/DaeguMogakko/BackEnd/security/provider/JwtProvider.java
@@ -0,0 +1,40 @@
+package DaeguMogakko.BackEnd.security.provider;
+
+import DaeguMogakko.BackEnd.security.jwt.JwtUtil;
+import DaeguMogakko.BackEnd.security.memberDetails.MemberDetails;
+import DaeguMogakko.BackEnd.security.memberDetails.MemberDetailsService;
+import DaeguMogakko.BackEnd.security.token.JwtAuthenticationToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider implements AuthenticationProvider {
+
+    private final JwtUtil jwtUtil;
+    private final MemberDetailsService memberDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String authToken = (String) authentication.getPrincipal();
+        jwtUtil.validateToken(authToken);
+        MemberDetails memberDetails = (MemberDetails) memberDetailsService.loadUserByUsername(authToken);
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(
+                memberDetails.getAuthorities(),
+                memberDetails.getUsername(),
+                null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        return new JwtAuthenticationToken(memberDetails.getAuthorities(), memberDetails.getUsername(), null);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.isAssignableFrom(JwtAuthenticationToken.class);
+    }
+}
---
+++ src/main/java/DaeguMogakko/BackEnd/security/token/JwtAuthenticationToken.java
@@ -0,0 +1,38 @@
+package DaeguMogakko.BackEnd.security.token;
+
+import java.util.Collection;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final String authToken;
+    private final Object principal;
+    private final Object credentials;
+
+    public JwtAuthenticationToken(String authToken) {
+        super(null);
+        this.authToken = authToken;
+        this.principal = null;
+        this.credentials = null;
+        setAuthenticated(false);
+    }
+
+    public JwtAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        super(authorities);
+        this.authToken = null;
+        this.principal = principal;
+        this.credentials = credentials;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+}
